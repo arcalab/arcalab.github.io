8db717b821c9293c3fd63be282d7ea5267870f24
Jose Proenca <jose.proenca@cs.kuleuven.be>
2016-01-14 14:25:12 +0000

    moved talks info to a separate file.

---
+++ talks.js
@@ -0,0 +1,27 @@
+var nextTalks = [{
+    "date":"27 Jan 2016",
+    "author": "Carlos Tavares",
+    "alink": "haslab.uminho.pt/ctavares/",
+    "title": "tba",
+    "location": "Room DI.2.13 @ 14h45"
+}];
+
+var prevTalks = [{
+    "date":"13 Jan 2016",
+    "author": "Renato Neves",
+    "alink": "alfa.di.uminho.pt/~nevrenato",
+    "title": "Coalgebraic Hybrid Automata",
+//     "location": "Room DI.2.13 @ 14h45"
+  },{
+    "date":"2 Dec 2015",
+    "author": "Alexandre Madeira",
+    "alink": "alfa.di.uminho.pt/~madeira",
+    "title": "From Abstract Requirements to Processes in the Same Logic",
+//     "location": "Talk: 14:00 - 14:30 @ A1, DI. Meeting: 14:30 - 15:30 @ 2.13, DI"
+  },{
+    "date": "19 Nov 2015",
+    "author": "José Proença",
+    "link": "jose.proenca.org", 
+    "title": "Typed Connector Families (and more)",
+    "slides": "content/TypedConnectorCalculus.pdf"
+}];

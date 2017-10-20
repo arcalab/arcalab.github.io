20e5401491ea5e4cd91325d81cfb1527cc7287df
Jose Proenca <jose@proenca.org>
2017-10-20 18:05:25 +0100

    starting to experiment with new structure and logo.

---
+++ publications.js
@@ -0,0 +1,39 @@
+/*
+supported fields:
+ - date     - when the talk takes/took place
+ - title    - of the paper
+ - authors  - of the paper
+ - venue    - where the paper was presented/published
+ - paper    - link to the paper
+ - doi      - doi of the paper
+*/
+
+
+var publications = {
+      2017: [
+        { "authors": "José Proença and Dave Clarke"
+        , "title" : "Typed Connector Families and their semantics"
+        , "venue" : "Science of Computer Programming"
+        , "doi" : "10.1016/j.scico.2017.03.002"
+        , "paper" : "http://jose.proenca.org/papers/connector-families/scp-cfam.pdf"},
+        { "authors": "Guillermina Cledou"
+        , "title" : "IFTA"
+        , "venue" : "FSEN"},
+        { "authors": "Renato Neves"
+        , "title" : "Hybrid Automata"}
+      ]
+    , 2016: [
+        { "authors": "Guillermina Cledou"
+        , "title" : "IFTA"
+        , "venue" : "FSEN"},
+        { "authors": "Renato Neves"
+        , "title" : "Hybrid Automata"}
+      ]
+    , 2015: [
+        { "authors": "Guillermina Cledou"
+        , "title" : "IFTA"
+        , "venue" : "FSEN"},
+        { "authors": "Renato Neves"
+        , "title" : "Hybrid Automata"}
+      ]
+};

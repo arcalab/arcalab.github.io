df0445e4b230ea6e419e16ae60be950bb053adc4
Jose Proenca <jose@proenca.org>
2018-11-27 15:41:21 +0000

    talk of Guillermina's work in Japan

--- talks.js
+++ talks.js
@@ -23,12 +23,18 @@ var AR  = '<a href="extra/img/photos/ar.JPG">Afonso Rodrigues</a>'
 var RC  = '<a href="extra/img/photos/rc.jpg">Rúben Cruz</a>'
 
 var nextTalks = [{
-    "date":"2 Nov 2018",
+    "date":"29 Nov 2018",
     "author":GC,
-    "title": "A Virtual Factory for Smart City Service Integration",
+    "title": "A Net-based Formal Framework for Causal Loop Diagrams",
+    "paper": "content/csdm2018.pdf",
     "location": "Room DI.2.31 @ 16h00"
 }];
 var prevTalks = [{
+    "date":"2 Nov 2018",
+    "author":GC,
+    "title": "A Virtual Factory for Smart City Service Integration",
+    // "location": "Room DI.2.31 @ 16h00"
+    },{
     "date":" 31 July 2018",
     "title": "A Metalanguage for Guarded Iteration",
     "alink": "www8.cs.fau.de/sergey",
